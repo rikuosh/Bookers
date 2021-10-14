f6ab20123aa4b7e12d2e484d148d489102a68dd7
EC2 Default User <ec2-user@ip-172-31-77-5.ec2.internal>
2021-10-14 05:23:23 +0000

    modified error

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get "/" => "home#top"
+  root to: "home#top"
   resources:books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
